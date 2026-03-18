468ba2eb0dcdbf1e914f934391e6ec6a8a771221
austindriggs <austind26@icloud.com>
2026-03-17 22:08:22 -0400

    initial get_logs() and display on flask home page

--- src/app.py
+++ src/app.py
@@ -1,25 +1,23 @@
-from flask import Flask
+# https://flask.palletsprojects.com/en/stable/quickstart/
+
+from flask import Flask, render_template
+import log
 
 app = Flask(__name__)
 
+@app.route('/about/')
+def about():
+    return render_template('about.html')
+
+
 @app.route('/')
-def home():
-    return '''
-            <style>
-                body { background-color: #121212; color: #e0e0e0; font-family: sans-serif; padding: 40px; line-height: 1.6; }
-                h1 { color: #00ffcc; }
-                .container { max-width: 800px; margin: auto; }
-            </style>
-            <div class="container">
-                <h1>LogChain: A Lightweight and Secure Docker Historian</h1>
-                <p>LogChain is a lightweight security tool that provides <strong>tamper-evident logging</strong> for Docker environments. 
-                It continuously monitors container logs, cryptographically chains each log entry to preserve an <strong>immutable record</strong>, 
-                and alerts administrators when suspicious activity or log tampering is detected.</p>
-                
-                <p>The system is designed for homelabs, edge devices, and small servers where maintaining trustworthy logs is critical 
-                but ease of use and flexibility is desired.</p>
-            </div>
-        '''
+def index():
+    container_logs = log.get_logs()
+    return render_template('index.html', logs=container_logs)
+
 
 if __name__ == '__main__':
+    # 0.0.0.0 allows the app to be accessible from outside the container
+    # Port 5000 is the default Flask port, but it can be changed if needed
+    # debug=True enables auto-reloading and better error messages during development, NOT production
     app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ src/log.py
@@ -0,0 +1,23 @@
+import docker
+import sys
+
+
+def remove_excluded_containers(containers):
+    # eventually this will get yaml stuff and then remove things
+    containers = [c for c in containers if c.name != 'logchain_web']
+    return containers
+
+
+def get_logs():
+    all_containers = docker.from_env().containers.list(all=True)
+    containers = remove_excluded_containers(all_containers)
+
+    all_logs = {}
+    for container in containers:
+        print(f"Getting logs for {container.name} ({container.id})")
+        try:
+            all_logs[container.id] = container.logs().decode('utf-8')
+        except Exception as e:
+            print(f"Error getting logs for {container.name}: {e}")
+        
+    return all_logs
